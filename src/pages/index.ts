28ee66741f60ce9839132162667e9c74c21189ed
Kyaw Wanna <mgkyawwanna@gmail.com>
2026-02-16 16:26:32 +0630

    Final: Sync login

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,3 @@
-// src/pages/index.ts
-
 // Core Identity & Auth
 export { default as EnterprisePortal } from './EnterprisePortal';
 export { default as Login } from './Login';
@@ -7,51 +5,56 @@ export { default as Dashboard } from './Dashboard';
 export { default as AdminDashboard } from './AdminDashboard';
 export { default as Unauthorized } from './Unauthorized';
 
-// Operations & Performance (Using Tables & Tabs)
+// Operations & Performance
 export { default as ReceivingBay } from './ReceivingBay';
 export { default as DispatchManagement } from './DispatchManagement';
 export { default as RegistrationQueue } from './RegistrationQueue';
 export { default as WarehouseDrop } from './WarehouseDrop';
-export { default as Operations } from './Operations';
+export { default as TrackingMapPage } from './TrackingMapPage';
+export { default as HubOperations } from './HubOperations';
+export { default as LabelActivation } from './LabelActivation';
+export { default as SubstationReceiving } from './SubstationReceiving';
 
 // Security & Audit
 export { default as AuditLogs } from './AuditLogs';
+export { default as AuditDashboard } from './AuditDashboard';
 export { default as RoleManagement } from './RoleManagement';
-export { default as ForcePasswordReset } from './ForcePasswordReset';
-export { default as AdminUsers } from './AdminUsers';
+// Removed ForcePasswordReset as it was causing build errors
+export { default as TagInventoryManagement } from './TagInventoryManagement';
+export { default as TagBatchManagement } from './TagBatchManagement';
 
-// Utility & Support
-export { default as OperationalManual } from './OperationalManual';
-export { default as Finance } from './Finance';
-export { default as HumanResources } from './HumanResources';
+// Support & Client Modules
+export { default as CustomerSupport } from './CustomerSupport';
+export { default as CustomerServiceDashboard } from './CustomerServiceDashboard';
+export { default as LiveChatInterface } from './LiveChatInterface';
+export { default as MerchantPortal } from './MerchantPortal';
+export { default as MerchantAnalytics } from './MerchantAnalytics';
+export { default as ShipmentRegistration } from './ShipmentRegistration';
+
+// Marketing
 export { default as Marketing } from './Marketing';
-export { default as Substation } from './Substation';
+export { default as MarketingDashboard } from './MarketingDashboard';
+export { default as CampaignManager } from './CampaignManager';
+
+// Administration
+export { default as AdminUsers } from './AdminUsers';
 export { default as UserManagement } from './UserManagement';
 export { default as SystemSettings } from './SystemSettings';
 export { default as Reports } from './Reports';
-export { default as TagBatchManagement } from './TagBatchManagement';
-export { default as TagInventoryManagement } from './TagInventoryManagement';
-export { default as AuditDashboard } from './AuditDashboard';
-export { default as MerchantPortal } from './MerchantPortal';
-export { default as ShipmentRegistration } from './ShipmentRegistration';
-export { default as MerchantAnalytics } from './MerchantAnalytics';
-export { default as MarketingDashboard } from './MarketingDashboard';
-export { default as CampaignManager } from './CampaignManager';
-export { default as CustomerPortal } from './CustomerPortal';
-export { default as CustomerSupport } from './CustomerSupport';
-export { default as PickupFlow } from './PickupFlow';
-export { default as DeliveryFlow } from './DeliveryFlow';
-export { default as LabelActivation } from './LabelActivation';
-export { default as SubstationReceiving } from './SubstationReceiving';
-export { default as TrackingMapPage } from './TrackingMapPage';
-export { default as DataEntryForm } from './DataEntryForm';
-export { default as CustomerServiceDashboard } from './CustomerServiceDashboard';
-export { default as LiveChatInterface } from './LiveChatInterface';
+export { default as Finance } from './Finance';
+export { default as HumanResources } from './HumanResources';
+
+// Utilities
 export { default as ChangePassword } from './ChangePassword';
 export { default as LandingPage } from './LandingPage';
-export { default as DownloadAPK } from './downloadAPK';
-export { default as HubOperations } from './HubOperations';
-export { default as ControlRoom } from './ControlRoom';
-export { default as CourierApp } from './CourierApp';
 export { default as ManualPage } from './ManualPage';
+export { default as DownloadAPK } from './downloadAPK';
+export { default as CourierApp } from './CourierApp';
+export { default as Operations } from './Operations';
+export { default as PickupFlow } from './PickupFlow';
+export { default as DeliveryFlow } from './DeliveryFlow';
 export { default as QROperations } from './QROperations';
+export { default as ControlRoom } from './ControlRoom';
+export { default as CustomerPortal } from './CustomerPortal';
+export { default as DataEntryForm } from './DataEntryForm';
+export { default as ApprovalQueue } from './ApprovalQueue';
